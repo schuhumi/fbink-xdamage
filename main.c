50ea09c4d2528e32f9b69f888024453e48077bcc
Simon Schumann <simon.schumann@web.de>
2019-09-19 11:13:19 +0000

    slight cleanup

--- main.c
+++ main.c
@@ -15,62 +15,63 @@ int main(int argc, char *argv[])
 {
     Display* display = XOpenDisplay(NULL);
     Window root = DefaultRootWindow(display);
-    XWindowAttributes attributes = {0};
-    XGetWindowAttributes(display, root, &attributes);
-    int width, height;
-    width = attributes.width;
-    height = attributes.height;
+
     //XDAMAGE INIT
     int damage_event, damage_error, test;
     test = XDamageQueryExtension(display, &damage_event, &damage_error);
     Damage damage = XDamageCreate(display, root, XDamageReportNonEmpty);
     
     XEvent event;
-    /* We might need a pointer to an XDamageEvent... */
-    XDamageNotifyEvent *dev;
+    XDamageNotifyEvent *devent;
     
     FBInkConfig fbink_cfg = { 0U };
-    fbink_cfg.is_verbose  = true;
-	// Flash to make stuff more obvious
+    
+	// optional flash/verbose to make stuff more obvious
 	fbink_cfg.is_flashing = false;
+    fbink_cfg.is_verbose  = true;
+    
     fbink_cfg.wfm_mode = WFM_GC16_FAST;
+    
     // Assume success, until shit happens ;)
 	int rv = EXIT_SUCCESS;
 
 	// Init FBInk
 	int fbfd = -1;
 	// Open framebuffer and keep it around, then setup globals.
-	if ((fbfd = fbink_open()) == ERRCODE(EXIT_FAILURE)) {
+	if ((fbfd = fbink_open()) == ERRCODE(EXIT_FAILURE))
+    {
 		fprintf(stderr, "Failed to open the framebuffer, aborting . . .\n");
 		rv = ERRCODE(EXIT_FAILURE);
 		goto cleanup;
 	}
-	if (fbink_init(fbfd, &fbink_cfg) == ERRCODE(EXIT_FAILURE)) {
+	if (fbink_init(fbfd, &fbink_cfg) == ERRCODE(EXIT_FAILURE))
+    {
 		fprintf(stderr, "Failed to initialize FBInk, aborting . . .\n");
 		rv = ERRCODE(EXIT_FAILURE);
 		goto cleanup;
 	}
-    while (1){
+    
+    while (1)
+    {
         
         XNextEvent(display,&event);
         
-        printf("Got event! type:%i\n", event.type);
+        if( fbink_cfg.is_verbose )
+            printf("Got event! type:%i\n", event.type);
 
         if( 1 )//event.type == XDamageNotify)
         {
-            dev = (XDamageNotifyEvent*)&event;
+            devent = (XDamageNotifyEvent*)&event;
             XserverRegion region = XFixesCreateRegion(display, NULL, 0);
-            XDamageSubtract(display, dev->damage, None, region);
+            XDamageSubtract(display, devent->damage, None, region);
             int count;
             XRectangle* area = XFixesFetchRegion(display, region, &count);
             if(area){
                 for(int i=0; i < count; i++){
                     XRectangle rect = area[i];
-                    printf("x:%hi y:%hi width:%hi height:%hi\n", rect.x, rect.y, rect.width, rect.height);
-                    fbink_refresh(fbfd, rect.y, rect.x, rect.width, rect.height, HWD_ORDERED, &fbink_cfg);
-                
-
-                    
+                    if( fbink_cfg.is_verbose )
+                        printf("Damaged area: x:%hi y:%hi width:%hi height:%hi\n", rect.x, rect.y, rect.width, rect.height);
+                    fbink_refresh(fbfd, rect.y, rect.x, rect.width, rect.height, HWD_ORDERED, &fbink_cfg);                 
                 }
                 XFree(area);
             }
@@ -82,15 +83,11 @@ int main(int argc, char *argv[])
     }
     XCloseDisplay(display);
         
-    	// Cleanup
+    
+    // Cleanup
 cleanup:
-	/*// Free potential dump data...
-	if (fbink_free_dump_data(&dump) == ERRCODE(EINVAL)) {
-		fprintf(stderr, "There was no dump data to release!\n");
-		rv = ERRCODE(EXIT_FAILURE);
-	}*/
-
-	if (fbink_close(fbfd) == ERRCODE(EXIT_FAILURE)) {
+	if (fbink_close(fbfd) == ERRCODE(EXIT_FAILURE))
+    {
 		fprintf(stderr, "Failed to close the framebuffer, aborting . . .\n");
 		rv = ERRCODE(EXIT_FAILURE);
 	}
